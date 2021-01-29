4ab9e7e6abc22752b0f373460f9545431e6b11e6
aman adinlid <95pinkpanda@gmail.com>
2021-01-29 18:57:02 +0100

    Practicing

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,36 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import se.lexicon.Interfaces.Action;
+import se.lexicon.Interfaces.Conditional;
+import se.lexicon.Moudle.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Tv", 2000, 2));
+        products.add(new Product("Phone", 3000, 0));
+        products.add(new Product("BTable", 200, 1));
+
+        Conditional conditional = p -> p.getStock() == 0;
+        Action action = p -> System.out.println(p);
+        process(products, conditional, action);
+        process(products, p -> p.getProdName().startsWith("B"), p -> System.out.println(p));
+
     }
+
+    public static void process(List<Product> productList, Conditional filter, Action action) {
+
+        for (Product product : productList) {
+            if (filter.test(product)) {
+                action.execute(product);
+            }
+        }
+    }
+
+
 }
